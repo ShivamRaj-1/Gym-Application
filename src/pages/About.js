f207cc178027b2472e77fb9bf6dd29ff67d76099
shivamdaksh404 <shivofficial05@gmail.com>
2023-04-18 13:57:18 +0530

    Home And NavBar Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <div className={styles.container}>
       <section className={styles.about}>
-      <iframe  src="https://www.youtube.com/embed/vei81-bZhG4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen='true'></iframe>
+      <iframe  src="https://www.youtube.com/embed/vei81-bZhG4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true}></iframe>
       <div className={styles.content}>
         <h2>About Us</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, recusandae nihil eius iusto quibusdam debitis rem ea! Veritatis placeat, possimus assumenda similique ipsum nulla aliquam magni cumque praesentium earum quisquam, fugiat quasi. Facilis cumque mollitia dolores nobis nesciunt quae libero?</p>
@@ -17,7 +17,7 @@ export default function About() {
       </div>
       </section>
       <section className={styles.choose}>
-        <iframe  src="https://www.youtube.com/embed/Rd3XHPk5me4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen='true'></iframe>
+        <iframe  src="https://www.youtube.com/embed/Rd3XHPk5me4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true}></iframe>
         <div className={styles.content}>
         <h2>Why Choose Us?</h2>
         <ul>
