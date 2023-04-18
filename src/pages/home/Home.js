45a1578a5ac80242b82545beb9cf7ef12e6ba95e
shivamdaksh404 <shivofficial05@gmail.com>
2023-04-18 13:08:36 +0530

    Update Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,74 +1,93 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import style from "./Home.module.css";
 import Button from "@mui/material/Button";
 import TrainingProgram from "../trainingProgram/TrainingProgram";
 import { slideData } from "../../utills/HomePageSliderData.js";
 import { useState } from "react";
-import { MdArrowForwardIos, MdArrowBackIosNew } from 'react-icons/md';
+import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
+import { height } from "@mui/system";
 
 export default function Home() {
-const [slideValue,setSlideValue] = useState(0);
+  const [slideValue, setSlideValue] = useState(0);
 
-
-const handleSlideChangeLeft =()=>{
-  setSlideValue((slideValue+1)%slideData.length )
-}
-const handleSlideChangeRight =()=>{
-  setSlideValue((slideValue-1) )
-}
-
+  const handleSlideChangeLeft = () => {
+    setSlideValue((slideValue + 1) % slideData.length);
+  };
+  const handleSlideChangeRight = () => {
+    setSlideValue(slideValue - 1);
+  };
 
   return (
     <Fragment>
-      {/* <ImageSlider text={slideData.text} /> */}
-      {/* <Slider />
-      <Slider />
-      <Slider /> */}
-      {/* {slideData.map((ele,index)=>{
-        return(
-          <Slider index={index}/>
-        )
-      })} */}
-      
-      <button onClick={handleSlideChangeLeft}><MdArrowBackIosNew/></button>
-          <Slider index={slideValue}/>
-      <button onClick={handleSlideChangeRight}><MdArrowForwardIos/></button>
-
-      <TrainingProgram />
+      <ImageSlider />
+      {/* <button onClick={handleSlideChangeLeft}>
+        <MdArrowBackIosNew />
+      </button>
+      <Slider index={slideValue} />
+      <button onClick={handleSlideChangeRight}>
+        <MdArrowForwardIos />
+      </button>
+      <TrainingProgram /> */}
     </Fragment>
   );
 }
 
 export function ImageSlider() {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentSlideIndex === 2) {
+        setCurrentSlideIndex(0);
+      } else {
+        setCurrentSlideIndex(currentSlideIndex + 1);
+      }
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [currentSlideIndex]);
+
+  const bgImageStyle = {
+    backgroundImage: `url(${slideData[currentSlideIndex].url})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "100%",
+  };
+
+  const goToNext = (currentSlideIndex) => {
+    setCurrentSlideIndex(currentSlideIndex);
+  };
+
   return (
-    <div className={style.container}>
-      <div className={style.textContainer}>
-        {slideData.map((ele) => {
-          return (
-            <div>
-              <h1 className={style.text}>
-                {ele.text}
-                {/* "Take care of your <span>body</span> it's <br />
-      the only place have to <span>live</span>." */}
-              </h1>
-              <Button variant="contained" color="success">
-                Join With Us
-              </Button>
-            </div>
-          );
-        })}
+    <div className={style.containerStyles}>
+      <div style={bgImageStyle}></div>
+      <div className={style.transparentBackground}></div>
+      <div className={style.description}>
+       
+          <h1>{slideData[currentSlideIndex].text}</h1>
+          <p>{slideData[currentSlideIndex].body}</p>
+          <Button variant="contained" color="success">
+            Join With Us
+          </Button>
+        
       </div>
+        <div className={style.carousal}>
+          {slideData.map((slideData, currentSlideIndex) => (
+            <span
+              key={currentSlideIndex}
+              onClick={() => goToNext(currentSlideIndex)}
+            ></span>
+          ))}
+        </div>
     </div>
   );
 }
 
-export function Slider({index}) {
+export function Slider({ index }) {
   return (
     <div className={style.container}>
-
       <div className={style.textContainer}>
         <h1 className={style.text}>
-        {slideData[index].text}
+          {slideData[index].text}
           {/* "Take care of your <span>body</span> it's <br />
           the only place have to <span>live</span>." */}
         </h1>
@@ -76,7 +95,6 @@ export function Slider({index}) {
           Join With Us
         </Button>
       </div>
-
     </div>
   );
 }
--- src/utills/HomePageSliderData.js
+++ src/utills/HomePageSliderData.js
@@ -1,13 +1,17 @@
-export const slideData =[{
-    text: `Take care of your body it's the only place have to live.`
+export const slideData = [
+  {
+    text: `Take care of your body it's the only place have to live.`,
+    url: "./flexibility.jpg",
+    body: "one",
   },
   {
-  text:'this is second slide'
+    text: "this is second slide",
+    url: "./cardio.jpg",
+    body: "two",
   },
   {
-    text:'this is Third Slide'
-  }
-  ]
-
-
-  
+    text: "this is Third Slide",
+    url: "./yoga.jpg",
+    body: "Three",
+  },
+];
